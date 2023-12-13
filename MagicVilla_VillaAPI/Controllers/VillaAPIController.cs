3ffcb4973b318ccc53dedfddfe2e435bf199e39c
Vasu <vasumendapara93@gmail.com>
2023-12-13 12:22:47 +0530

    Update VillaAPIController.cs
    Get Individual villa

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -11,9 +11,15 @@ namespace MagicVilla_VillaAPI.Controllers
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<VillaDTO> GetAll()
+        public IEnumerable<VillaDTO> GetVillas()
         {
             return VillaStore.villaList;
+        }
+
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u=>u.Id==id);
         }
     }
 }
