f9b92377c54c3923d463b5df96ecebb3ab4b6d33
Vasu <vasumendapara93@gmail.com>
2023-12-13 12:12:40 +0530

    Added VillaDTO and VillaStore for storing villa data.

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,20 +1,19 @@
-﻿using MagicVilla_VillaAPI.Models;
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
+    //[Route("api/[controller]")]
     [Route("api/VillaAPI")]
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<Villa> GetAll()
+        public IEnumerable<VillaDTO> GetAll()
         {
-            return new List<Villa>
-            {
-                new Villa{Id=1,Name="Pool View"},
-                new Villa{Id=2,Name="Beach View"}
-            };
+            return VillaStore.villaList;
         }
     }
 }
