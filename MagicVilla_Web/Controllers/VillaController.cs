14da6a485f93b86584418e6e1cd3eafb51c39747
Vasu <vasumendapara93@gmail.com>
2023-12-21 11:56:29 +0530

    Update Villa

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -4,6 +4,7 @@ using MagicVilla_Web.Models.Dto;
 using MagicVilla_Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Reflection;
 
 namespace MagicVilla_Web.Controllers
 {
@@ -26,6 +27,7 @@ namespace MagicVilla_Web.Controllers
             }
             return View(list);
         }
+
 		public async Task<IActionResult> CreateVilla()
 		{
 			return View();
@@ -37,6 +39,31 @@ namespace MagicVilla_Web.Controllers
 			if(ModelState.IsValid)
             {
 				var response = await _villaService.CreateAsync<APIResponse>(model);
+				if (response != null && response.IsSuccess)
+				{
+					return RedirectToAction(nameof(IndexVilla));
+				}
+			}
+			return View(model);
+		}
+
+		public async Task<IActionResult> UpdateVilla(int villaId)
+		{
+			var response = await _villaService.GetAsync<APIResponse>(villaId);
+			if (response != null && response.IsSuccess)
+			{
+				VillaDTO model = JsonConvert.DeserializeObject<VillaDTO>(Convert.ToString(response.Result));
+				return View(_mapper.Map<VillaUpdateDTO>(model));
+			}
+			return NotFound();
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> UpdateVilla(VillaUpdateDTO model)
+		{
+			if (ModelState.IsValid)
+			{
+				var response = await _villaService.UpdateAsync<APIResponse>(model);
 				if (response != null && response.IsSuccess)
 				{
 					return RedirectToAction(nameof(IndexVilla));
