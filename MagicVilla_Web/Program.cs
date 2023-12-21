41ec8c71777b09f945934d9603328186c2f5a38e
Vasu <vasumendapara93@gmail.com>
2023-12-21 12:15:29 +0530

    Add services villanumberservice and ivillanumberservice

--- MagicVilla_Web/Program.cs
+++ MagicVilla_Web/Program.cs
@@ -7,8 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 builder.Services.AddAutoMapper(typeof(MappingConfig));
+
 builder.Services.AddHttpClient<IVillaService, VillaService>();
 builder.Services.AddScoped<IVillaService, VillaService>();
+
+builder.Services.AddHttpClient<IVillaNumberService, VillaNumberService>();
+builder.Services.AddScoped<IVillaNumberService, VillaNumberService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
